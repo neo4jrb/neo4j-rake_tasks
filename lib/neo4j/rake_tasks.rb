f8915234ca3cfa246ab3516bbcf5e109294a43f2
Brian Underwood <ml+github@semi-sentient.com>
2015-08-02 09:20:40 -0300

    Load rake tasks automatically when gem is required

--- lib/neo4j/rake_tasks.rb
+++ lib/neo4j/rake_tasks.rb
@@ -1,5 +1,6 @@
 
 require 'neo4j/rake_tasks/starnix_server_manager'
 require 'neo4j/rake_tasks/windows_server_manager'
+load 'neo4j/rake_tasks/neo4j_server.rake'
 
 require 'neo4j/rake_tasks/railtie' if defined?(Rails)
